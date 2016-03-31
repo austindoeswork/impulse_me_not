cb24b400e51142b9614afda27e7443a99fc197c0
austindoeswork <wilsonaustin17@gmail.com>
2016-03-31 12:15:26 -0400

    added chrome extension basics

---
+++ chrome/hello.js
@@ -0,0 +1,12 @@
+// hello.js
+
+
+
+$(function() {
+    console.log( "ready!" );
+	console.log("hello world");
+	// $("#brand").get(0).click();
+	htmlstring = $("#submit\\.add-to-cart-announce").html("poopies");
+
+
+});
